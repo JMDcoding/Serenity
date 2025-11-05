19ab7fe4c8715ca7394c0044801a63c8b08ee8de
JMDcoding <jeanmarcdussaud@gmail.com>
2025-11-05 14:02:56 +0100

    test decouverte C++ et C

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "Serenity_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Bienvenue dans Serenity 🌿" << endl;
+    return 0;
+}
+
+
+
+
+
